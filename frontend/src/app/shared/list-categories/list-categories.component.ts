8d6e67464f5ef8c153017033fe5785316ecf62dd
alex <alex.mart.juan@gmail.com>
2024-09-26 00:40:10 +0200

    FrontEnd. Home. Creación componente "list-categories" con sus archivos, inserción list-categories en home.component.html y visualización contenido de list-categories en web.

---
+++ frontend/src/app/shared/list-categories/list-categories.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-categories',
+  templateUrl: './list-categories.component.html',
+  styleUrl: './list-categories.component.css'
+})
+export class ListCategoriesComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ListCategoriesComponent } from './list-categories/list-categories.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListCategoriesComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
-  ]
+  ],
+  exports: [
+      ListCategoriesComponent
+  ],
 })
 export class SharedModule { }
