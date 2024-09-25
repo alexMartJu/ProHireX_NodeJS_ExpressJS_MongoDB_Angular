ed08b3bdc9607e1005a9bafc8d998e9942650e0a
alex <alex.mart.juan@gmail.com>
2024-09-26 01:48:54 +0200

    FrontEnd. Shop. Routing Shop y comprobación routing en web a través del HTML.

--- frontend/src/app/shop/shop-routing.module.ts
+++ frontend/src/app/shop/shop-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShopComponent,
+    resolve: {},
+  },
+  {
+    path: 'categories/:slug',
+    component: ShopComponent,
+    resolve: {},
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/shop/shop.component.ts
+++ frontend/src/app/shop/shop.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
-export class ShopComponent {
-
+export class ShopComponent implements OnInit {
+  constructor(){ }
+  
+  ngOnInit(): void { }
 }
