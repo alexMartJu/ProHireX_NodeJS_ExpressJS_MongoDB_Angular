db61567908b4a307abbc1ddfabf2056a157e04b4
alex <alex.mart.juan@gmail.com>
2024-09-26 09:04:28 +0200

    FrontEnd. Details. Modificacion details.components.ts y añadido HTML y CSS a Details para conseguir la vista del Details en web.

--- frontend/src/app/details/details.component.ts
+++ frontend/src/app/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Job } from '../core/models/job.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void {}
+  job!: Job;
+  slug!: string | null;
+
+  constructor(private route : ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(
+      (data: any) => {
+          this.slug = data.job.jobs.slug;
+          this.job = data.job.jobs;
+    });
+  }
 }
