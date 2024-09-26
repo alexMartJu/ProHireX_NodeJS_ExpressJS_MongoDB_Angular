0bd783e5002f19dd90552a7481c697c14f0b6ede
alex <alex.mart.juan@gmail.com>
2024-09-26 07:45:37 +0200

    FrontEnd. Shop. Creación componente "list-jobs" con sus archivos, inserción list-jobs en shop.component.html y visualización contenido de list-jobs en web.

---
+++ frontend/src/app/shared/list-jobs/list-jobs.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-jobs',
+  templateUrl: './list-jobs.component.html',
+  styleUrl: './list-jobs.component.css'
+})
+export class ListJobsComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,20 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ListCategoriesComponent } from './list-categories/list-categories.component';
 import { CardCategoryComponent } from './card-category/card-category.component';
+import { ListJobsComponent } from './list-jobs/list-jobs.component';
 
 
 
 @NgModule({
   declarations: [
     ListCategoriesComponent,
-    CardCategoryComponent
+    CardCategoryComponent,
+    ListJobsComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-      ListCategoriesComponent
+      ListCategoriesComponent,
+      CardCategoryComponent,
+      ListJobsComponent
   ],
 })
 export class SharedModule { }
