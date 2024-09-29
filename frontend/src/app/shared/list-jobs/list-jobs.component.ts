fca5ed6c3ebe59691f4581abb9668a9c4e58302d
alex <alex.mart.juan@gmail.com>
2024-09-29 10:02:40 +0200

    FrontEnd. Filters. Modificación list-jobs.component.ts para manejar filtros en la ruta y obtener categorías.

--- frontend/src/app/shared/list-jobs/list-jobs.component.ts
+++ frontend/src/app/shared/list-jobs/list-jobs.component.ts
@@ -17,41 +17,72 @@ export class ListJobsComponent implements OnInit {
   slug_Category!: string | null;
   listCategories: Category[] = [];
   filters = new Filters();
+  routeFilters!: string | null;
 
   constructor(private jobService: JobService,
     private ActivatedRoute: ActivatedRoute,
-    private CategoryService: CategoryService) {}
+    private CategoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.slug_Category = this.ActivatedRoute.snapshot.paramMap.get('slug');
-      console.log(this.slug_Category);
+    console.log(this.slug_Category);
+    // console.log(this.ActivatedRoute.snapshot.paramMap.get('filters'));
+    this.routeFilters = this.ActivatedRoute.snapshot.paramMap.get('filters');
 
-      if(this.slug_Category !== null) {
-        this.get_products_by_cat();
-      }else{
-        this.get_list_filtered(this.filters);
-      }
+    this.getListForCategory();
+
+    // - Si hay un slug de categoría, obtiene los trabajos filtrados por esa categoría.
+    // - Si no hay slug de categoría pero hay filtros en la ruta, actualiza los filtros y obtiene los trabajos filtrados.
+    // - Si no hay ni slug de categoría ni filtros, obtiene todos los trabajos disponibles.
+    if (this.slug_Category !== null) {
+      this.get_products_by_cat();
+    } else if (this.routeFilters !== null) {
+      this.refreshRouteFilter();
+      this.get_list_filtered(this.filters);
+    } else {
+      this.get_list_filtered(this.filters);
+    }
   }
 
   get_products_by_cat(): void {
-  
+
     if (this.slug_Category !== null) {
       console.log("entro aqui");
       this.jobService.getJobsByCategory(this.slug_Category).subscribe(
         (data: any) => {
           this.job = data.jobs;
           console.log(data.jobs);
-      });
+        });
     }
   }
 
   get_list_filtered(filters: Filters) {
     this.filters = filters;
     console.log("entro aqui2");
-      this.jobService.get_products_filter(filters).subscribe(
-        (data: any) => {
-          this.job = data.jobs;
-          console.log(this.job);
+    this.jobService.get_products_filter(filters).subscribe(
+      (data: any) => {
+        this.job = data.jobs;
+        console.log(this.job);
       });
+  }
+
+  //   getListCategory() --> Obtiene todas las categorías disponibles para la selección.
+  //  Llama al servicio CategoryService para obtener la lista de categorías
+  //  y almacena la respuesta en la propiedad listCategories.
+  getListForCategory() {
+    this.CategoryService.all_categories_select().subscribe(
+      (data: any) => {
+        this.listCategories = data.categories;
+      }
+    );
+  }
+
+  refreshRouteFilter() {
+    this.routeFilters = this.ActivatedRoute.snapshot.paramMap.get('filters');
+    if (typeof (this.routeFilters) == "string") {
+      this.filters = JSON.parse(atob(this.routeFilters));
+    } else {
+      this.filters = new Filters();
+    }
   }
 }
