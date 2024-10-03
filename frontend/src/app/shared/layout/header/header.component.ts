1f2d0c2c01c1fbc69e655ba719a3885ada751348
alex <alex.mart.juan@gmail.com>
2024-10-04 01:38:56 +0200

    FrontEnd. Auth. Refactorizar HeaderComponent para mostrar diferentes navegaciones según el estado de autenticación del usuario.

--- frontend/src/app/shared/layout/header/header.component.ts
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import { User} from '../../../core/models/auth.model';
+import { UserService} from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +9,37 @@ import { Component, OnInit} from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit{
-  constructor() { }
 
-  ngOnInit(): void { }
+  logged: Boolean = false;
+
+  constructor(private userService: UserService,
+  private cd: ChangeDetectorRef,
+  private router: Router) { }
+
+  currentUser!: User;
+
+  ngOnInit(): void { 
+    this.userService.isAuthenticated.subscribe(
+      (data) => {
+        this.logged = data;
+        // console.log(data);
+        // console.log(this.logged);
+        // this.cd.markForCheck();
+      }
+    );
+    this.userService.currentUser.subscribe(
+      (userData) => {
+        // console.log(userData);
+        this.currentUser = userData;
+        // console.log(this.currentUser);
+        
+        this.cd.markForCheck();
+      }
+    );
+  }
+
+  logout() {
+    this.userService.purgeAuth();
+    this.router.navigateByUrl('/');
+  }
 }
