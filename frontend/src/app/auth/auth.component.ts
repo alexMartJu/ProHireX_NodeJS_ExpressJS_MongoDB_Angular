ababd519b873d7a67fa93ba1da8a4ba55782209f
alex <alex.mart.juan@gmail.com>
2024-10-04 01:04:35 +0200

    FrontEnd. Auth. Implementación AuthComponent para gestionar formularios de login y register, incluyendo validación y redirección tras autenticación exitosa.

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  authType: string = '';
+  title: String = '';
+  isSubmitting = false;
+  authForm: FormGroup;
+  user!: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService,
+    private fb: FormBuilder,
+    private cd: ChangeDetectorRef
+  ) {
+    // use FormBuilder to create a form group
+    this.authForm = this.fb.group({
+      'email': ['', Validators.required],
+      'password': ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.route.url.subscribe(data => {
+      // Get the last piece of the URL (it's either 'login' or 'register')
+      this.authType = data[data.length - 1].path;
+      // Set a title for the page accordingly
+      this.title = (this.authType === 'login') ? 'Sign in' : 'Sign up';
+      // add form control for username if this is the register page
+      if (this.authType === 'register') {
+        this.authForm.addControl('username', new FormControl());
+      }
+      this.cd.markForCheck();
+    });
+  }
+
+  submitForm() {
+    this.isSubmitting = true;
+    // this.errors = {errors: {}};
+
+    const credentials = this.authForm.value;
+    // console.log('Sending credentials:', this.user); 
+    // console.log(this.authType);
+    this.userService
+    .attemptAuth(this.authType, credentials)
+    .subscribe(
+      data => this.router.navigateByUrl('/'),
+      err => {
+        // this.errors = err;
+        this.isSubmitting = false;
+        this.cd.markForCheck();
+      }
+    );
+  }
 }
