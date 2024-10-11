c00fe3a02f76b5747b00433f414d37f1f6ade2e8
alex <alex.mart.juan@gmail.com>
2024-10-11 11:27:57 +0200

    FrontEnd. RefreshToken. Actualización de JwtService y authModel para soporte de Access y Refresh Tokens.

--- frontend/src/app/core/models/auth.model.ts
+++ frontend/src/app/core/models/auth.model.ts
@@ -2,6 +2,7 @@ export interface User {
     uuid: string;
     email: string;
     token: string;
+    refreshToken: string;
     username: string;
     bio: string;
     image: string;
--- frontend/src/app/core/services/jwt.service.ts
+++ frontend/src/app/core/services/jwt.service.ts
@@ -5,15 +5,33 @@ import { Injectable } from '@angular/core';
 })
 export class JwtService {
 
-  getToken(): String {
-    return window.localStorage['jwtToken'];
+  // Obtener el Access Token
+  getToken(): string | null {
+    return window.localStorage['accessToken'];
   }
 
+  // Guardar el Access Token
   saveToken(token: String) {
-    window.localStorage['jwtToken'] = token;
+    window.localStorage['accessToken'] = token;
   }
 
+  // Eliminar el Access Token
   destroyToken() {
-    window.localStorage.removeItem('jwtToken');
+    window.localStorage.removeItem('accessToken');
+  }
+
+  // Obtener el Refresh Token
+  getRefreshToken(): string {
+    return window.localStorage['refreshToken'];
+  }
+
+  // Guardar el Refresh Token
+  saveRefreshToken(token: string) {
+    window.localStorage['refreshToken'] = token;
+  }
+
+  // Eliminar el Refresh Token
+  destroyRefreshToken() {
+    window.localStorage.removeItem('refreshToken');
   }
 }
