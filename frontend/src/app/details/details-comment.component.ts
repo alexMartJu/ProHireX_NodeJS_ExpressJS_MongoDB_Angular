da9c43efecdf6f87799590ee7c754e30d07fb2b4
alex <alex.mart.juan@gmail.com>
2024-10-16 23:22:31 +0200

    FrontEnd. Follow_Likes_Comments. Añadida lógica para manejar comentarios y permisos de modificación en DetailsCommentComponent.

--- frontend/src/app/details/details-comment.component.ts
+++ frontend/src/app/details/details-comment.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { Comments, User, UserService } from '../core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-details-comment',
   templateUrl: './details-comment.component.html',
   styleUrl: './details-comment.component.css'
 })
-export class DetailsCommentComponent {
+export class DetailsCommentComponent implements OnInit, OnDestroy {
 
+  constructor(
+    private userService: UserService,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  private subscription!: Subscription;
+
+  @Input() comment!: Comments;
+  @Output() deleteComment = new EventEmitter<boolean>();
+
+  canModify!: boolean;
+
+  ngOnInit() {
+    // Load the current user's data
+    this.subscription = this.userService.currentUser.subscribe(
+      (userData: User) => {
+        this.canModify = (userData.username === this.comment.author.username);
+        console.log(this.canModify);
+        this.cd.markForCheck();
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  deleteClicked() {
+    this.deleteComment.emit(true);
+  }
 }
