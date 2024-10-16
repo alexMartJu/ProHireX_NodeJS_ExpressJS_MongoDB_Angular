a46095da5c1eac3b60eded007fc5a47dc9847732
alex <alex.mart.juan@gmail.com>
2024-10-16 16:25:51 +0200

    FrontEnd. Follow_Likes_Comments. Implementada lógica de seguimiento en FollowButtonComponent.

--- frontend/src/app/shared/buttons/follow-button.component.ts
+++ frontend/src/app/shared/buttons/follow-button.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Profile, ProfileService, UserService } from '../../core';
+import { concatMap ,  tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-follow-button',
@@ -7,4 +12,54 @@ import { Component } from '@angular/core';
 })
 export class FollowButtonComponent {
 
+  constructor(
+    private profilesService: ProfileService,
+    private router: Router,
+    private userService: UserService,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  @Input() profile!: Profile;
+  @Output() toggle = new EventEmitter<boolean>();
+  isSubmitting = false;
+
+  toggleFollowing() {
+    this.isSubmitting = true;
+    // TODO: remove nested subscribes, use mergeMap
+
+    this.userService.isAuthenticated.pipe(concatMap(
+      (authenticated) => {
+        // Not authenticated? Push to login screen
+        if (!authenticated) {
+          this.router.navigateByUrl('/auth/login');
+          return of(null);
+        }
+
+        // Follow this profile if we aren't already
+        if (!this.profile.following) {
+          return this.profilesService.follow(this.profile.username)
+          .pipe(tap(
+            data => {
+              this.isSubmitting = false;
+              this.toggle.emit(true);
+            },
+            err => this.isSubmitting = false
+          ));
+
+        // Otherwise, unfollow this profile
+        } else {
+          return this.profilesService.unfollow(this.profile.username)
+          .pipe(tap(
+            data => {
+              this.isSubmitting = false;
+              this.toggle.emit(false);
+            },
+            err => this.isSubmitting = false
+          ));
+        }
+      }
+    )).subscribe(() => {
+      this.cd.markForCheck();
+    });
+  }
 }
