51867d609f75d905715b49a7b22a344af8c54f18
alex <alex.mart.juan@gmail.com>
2024-10-17 00:25:51 +0200

    FrontEnd. Follow_Likes_Comments. Modificado FollowListComponent para incluir propiedades de entrada.

--- frontend/src/app/shared/follow-list/follow-list.component.ts
+++ frontend/src/app/shared/follow-list/follow-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Follower_follow } from '../../core';
 
 @Component({
   selector: 'app-follow-list',
   templateUrl: './follow-list.component.html',
-  styleUrl: './follow-list.component.css'
+  styleUrls: ['./follow-list.component.css']
 })
-export class FollowListComponent {
+export class FollowListComponent implements OnInit {
+  @Input() followersss: Follower_follow = {} as Follower_follow;
+  @Input() follow: Follower_follow = {} as Follower_follow;
+  @Input() pages_profile!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // console.log(this.pages_profile);
+  }
 
 }
