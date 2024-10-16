d8dc4b12a2d5fed9dab2fa07d0ecfebf23c67465
alex <alex.mart.juan@gmail.com>
2024-10-16 15:43:30 +0200

    FrontEnd. Follow_Likes_Comments. Añadida lógica para manejar la funcionalidad de favoritos en el componente FavoriteButton.

--- frontend/src/app/shared/buttons/favorite-button.component.ts
+++ frontend/src/app/shared/buttons/favorite-button.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Job, JobService, UserService } from '../../core';
+import { of } from 'rxjs';
+import { concatMap ,  tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-favorite-button',
@@ -7,4 +12,54 @@ import { Component } from '@angular/core';
 })
 export class FavoriteButtonComponent {
 
+  constructor(
+    private JobService: JobService,
+    private router: Router,
+    private userService: UserService,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  @Input() jobs: Job = {} as Job;
+  @Output() toggle = new EventEmitter<boolean>();
+  isSubmitting = false;
+
+  toggleFavorite() {
+    this.isSubmitting = true;
+
+    this.userService.isAuthenticated.pipe(concatMap(
+      (authenticated) => {
+        // Not authenticated? Push to login screen
+        if (!authenticated) {
+          this.router.navigateByUrl('/auth/login');
+          return of(null);
+        }
+
+        // Favorite the article if it isn't favorited yet
+        if (!this.jobs.favorited) {
+          return this.JobService.favorite(this.jobs.slug)
+          .pipe(tap(
+            data => {
+              this.isSubmitting = false;
+              this.toggle.emit(true);
+            },
+            err => this.isSubmitting = false
+          ));
+
+        // Otherwise, unfavorite the article
+        } else {
+          return this.JobService.unfavorite(this.jobs.slug)
+          .pipe(tap(
+            data => {
+              this.isSubmitting = false;
+              this.toggle.emit(false);
+            },
+            err => this.isSubmitting = false
+          ));
+        }
+
+      }
+    )).subscribe(() => {
+      this.cd.markForCheck();
+    });
+  }
 }
