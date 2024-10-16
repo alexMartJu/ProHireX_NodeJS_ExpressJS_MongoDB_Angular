bec80eca82c7bea616af4b3dce2ed05e3c8c65fd
alex <alex.mart.juan@gmail.com>
2024-10-16 15:15:12 +0200

    BackEnd. Follow_Likes_Comments. Añadidas rutas para seguir/dejar de seguir usuarios y obtener perfil de usuario.

--- backend/app/routes/profile.routes.js
+++ backend/app/routes/profile.routes.js
@@ -2,7 +2,17 @@ module.exports = (app) => {
     
     const profileController = require('../controllers/profile.controller.js');
     const verifyJWTOptional = require('../middleware/verifyJWTOptional');
+    const verifyJWT = require('../middleware/verifyJWT');
 
     // Get profile - authentication optional
     app.get('/:username', verifyJWTOptional, profileController.getProfile);
+
+    // Follow a user
+    app.post('/:username/follow', verifyJWT, profileController.followUser);
+
+    // unfollow a user
+    app.delete('/:username/follow', verifyJWT, profileController.unFollowUser);
+
+    // Get user profile
+    app.get('/profile/:username', verifyJWTOptional, profileController.getProfile_User);
 }
