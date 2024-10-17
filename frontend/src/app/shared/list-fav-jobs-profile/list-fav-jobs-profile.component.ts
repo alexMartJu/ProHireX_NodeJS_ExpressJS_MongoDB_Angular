3d1d1b39218f845c92ced2e8a72aa589b39177af
alex <alex.mart.juan@gmail.com>
2024-10-17 07:50:19 +0200

    FrontEnd. Follow_Likes_Comments. Agreagadas propiedades de entrada y lógica para gestionar trabajos favoritos en list-fav-jobs-profile.

--- frontend/src/app/shared/list-fav-jobs-profile/list-fav-jobs-profile.component.ts
+++ frontend/src/app/shared/list-fav-jobs-profile/list-fav-jobs-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Job } from '../../core/models/job.model';
 
 @Component({
   selector: 'app-list-fav-jobs-profile',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './list-fav-jobs-profile.component.css'
 })
 export class ListFavJobsProfileComponent {
+  @Input() job: Job = {} as Job;
+  @Input() isUser: boolean = false;
 
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  onToggleFavorite(favorited: boolean) {
+    this.job.favorited = favorited;
+
+    if (favorited) {
+      this.job.favoritesCount++;
+    } else {
+      this.job.favoritesCount--;
+    }
+  }
 }
