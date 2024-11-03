f29cba8b611900d4b0decbb7cc275b4478a28a75
alex <alex.mart.juan@gmail.com>
2024-11-03 13:28:58 +0100

    FrontEnd. Mejorada la lógica de autenticación y visualización de encabezados en AppComponent.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { UserService, UserEnterpriseService } from './core';
+import { UserService, UserEnterpriseService, UserAdminService } from './core';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +10,34 @@ export class AppComponent {
   title = 'frontend';
   isCompanyUser = false;
   isLoading = true;
+  isAdminUser = false;
 
   constructor(private userService: UserService,
-    private userEnterpriseService: UserEnterpriseService
+    private userEnterpriseService: UserEnterpriseService,
+    private userAdminService: UserAdminService
   ) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this.userService.populate();
     this.userEnterpriseService.populate();
+    this.userAdminService.populate();
+    // Usamos un contador para saber cuándo todos los estados han sido evaluados
+  let completedRequests = 0;
     this.userEnterpriseService.isAuthenticated.subscribe(isAuthenticated => {
       this.isCompanyUser = isAuthenticated;
-      this.isLoading = false;
+      completedRequests++;
+      if (completedRequests === 2) {
+        this.isLoading = false;
+      }
       console.log(this.isCompanyUser);
+    });
+    this.userAdminService.isAuthenticated.subscribe(isAuthenticated => {
+      this.isAdminUser = isAuthenticated;
+      completedRequests++;
+      if (completedRequests === 2) {
+        this.isLoading = false;
+      }
     });
   };
 }
